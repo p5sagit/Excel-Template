64f0fd9a6b4c226919443ca6b8111f0cc225204f
Rob Kinyon <rob.kinyon@gmail.com>
2005-05-26 14:57:07 +0000

    Will use 3-arg open(), if available

--- lib/Excel/Template.pm
+++ lib/Excel/Template.pm
@@ -6,7 +6,7 @@ BEGIN {
     use Excel::Template::Base;
     use vars qw ($VERSION @ISA);
 
-    $VERSION  = '0.24';
+    $VERSION  = '0.25';
     @ISA      = qw( Excel::Template::Base );
 }
 
@@ -202,9 +202,17 @@ sub parse_xml
  
         push @parms, Base => $dirname;
 
-        open( INFILE, "<$file" )
-            || die "Cannot open '$file' for reading: $!\n";
-
+        eval q{
+            open( INFILE, '<', $file )
+                || die "Cannot open '$file' for reading: $!\n";
+        }; if ( $@ ) {
+           if ( $@ =~ /Too many arguments for open/ ) {
+                open( INFILE, "< $file" )
+                    || die "Cannot open '$file' for reading: $!\n";
+            } else {
+                die $@;
+            }
+        }
     }
 
     my $parser = XML::Parser->new( @parms );
